658c25e4438e0a41dc84e77df54645c67460f771
Wali Khan <wali.m.khan@emory.edu>
2022-02-20 23:21:03 -0800

    Update FractalMickeyMouse2.java

--- CS170/hw4/FractalMickeyMouse2.java
+++ CS170/hw4/FractalMickeyMouse2.java
@@ -15,12 +15,13 @@ public class FractalMickeyMouse{
 		t.forward(r);
 		t.left(90);
 		t.pendown();
-        for(int i = 0; i < 50; i++){
-   
-       		t.forward(r* Math.PI / 25);
-           	t.left(360.0/50);       
-        }
-        t.penup();
+		
+		for(int i = 0; i < 50; i++){
+			t.forward(r* Math.PI / 25);
+			t.left(360.0/50);       
+		}
+		
+		t.penup();
 		t.left(90);
 		t.forward(r);
 		t.right(90);
@@ -28,64 +29,68 @@ public class FractalMickeyMouse{
 		t.left(90);
 		t.forward(r/3);
 		t.pendown();
+		
 		for(int i = 0; i < 50; i++){
-   
-       		t.forward(r/10* Math.PI / 25);
-           	t.right(360.0/50);       
-        }
-        t.penup();
-        t.right(90);
-        t.forward(1);
-        t.color(0,0,0);
-        t.fill();
-        t.backward(1);
+       			t.forward(r/10* Math.PI / 25);
+           		t.right(360.0/50);       
+		}
+		
+		t.penup();
+		t.right(90);
+		t.forward(1);
+		t.color(0,0,0);
+		t.fill();
+		t.backward(1);
+		t.left(180);
+		t.penup();
+		t.forward(r/2);
+		t.right(90);
+		t.pendown();
+		
+		for(int i = 0; i < 50; i++){
+			t.forward(r/10* Math.PI / 25);
+			t.left(360.0/50);       
+		}
+		
+		t.penup();
+		t.left(90);
+		t.forward(1);
+		t.color(0,0,0);
+		t.fill();
+		t.backward(1);
+		t.left(90);
+		t.forward(r/3);
+		t.left(90);
+		t.forward(r/3);
+		t.left(90);
+		t.pendown();
+		
+		for(int i = 0; i < 50; i++){
 
-        t.left(180);
-        t.penup();
-        t.forward(r/2);
-        t.right(90);
-        t.pendown();
-        for(int i = 0; i < 50; i++){
-   
-       		t.forward(r/10* Math.PI / 25);
-           	t.left(360.0/50);       
-        }
-        t.penup();
-        t.left(90);
-        t.forward(1);
-        t.color(0,0,0);
-        t.fill();
-        t.backward(1);
-        t.left(90);
-        t.forward(r/3);
-        t.left(90);
-        t.forward(r/3);
-        t.left(90);
-        t.pendown();
-        for(int i = 0; i < 50; i++){
-   
-       		t.forward(r/15* Math.PI / 25);
-           	t.left(360.0/50);       
-        }
-        t.penup();
-        t.left(90);
-        t.forward(1);
-        t.color(0,0,0);
-        t.fill();
-        t.backward(1);
+			t.forward(r/15* Math.PI / 25);
+			t.left(360.0/50);       
+		}
+		
+		t.penup();
+		t.left(90);
+		t.forward(1);
+		t.color(0,0,0);
+		t.fill();
+		t.backward(1);
+		t.right(180);
+		t.forward(r/1.5);
+		t.right(90);
+		t.pendown();
+		
+		for(int i = 0; i < 50; i++){
 
-        t.right(180);
-        t.forward(r/1.5);
-        t.right(90);
-        t.pendown();
-        for(int i = 0; i < 50; i++){
-   
-       		t.forward(r/1.25* Math.PI / 50);
-           	t.right(180.0/50);       
-        }
-        t.right(90);
-        t.penup();
-        t.forward(r/1.2);
+			t.forward(r/1.25* Math.PI / 50);
+			t.right(180.0/50);       
+		}
+		
+		t.right(90);
+		t.penup();
+		t.forward(r/1.2);
 	}
 
 
@@ -93,7 +98,7 @@ public class FractalMickeyMouse{
 
 		if (levels == 0){
 			mickeyFace(t,r);
-		}else{
+		} else {
 			mickeyFace(t,r);
 			t.penup();
 			t.left(45);
