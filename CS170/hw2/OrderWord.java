816acdc9d80397edf048051db2a449c6d5ba9c3f
Wali Khan <wali.m.khan@emory.edu>
2022-02-21 00:01:12 -0800

    Update OrderWord.java

--- CS170/hw2/OrderWord.java
+++ CS170/hw2/OrderWord.java
@@ -5,25 +5,22 @@ public class OrderWord {
 		System.out.println(isOrdered("AAaaayy"));
 		System.out.println(isOrdered(""));
 		System.out.println(isOrdered("A"));
-
-
 	}
 
 	public static boolean isOrdered(String n) { // this is the method which returns a boolean value and takes a parameter of some type of String. 
 		n = n.toLowerCase();
-	if (n.length() == 0) { // if the String is empty then the result is true 
-	return true;
-	}
-	if (n.length() == 1) { // if the String is just one char then the result is true 
-	return true;
-	}
-	for (int i = 0; i < n.length() - 1; i++) {
-	if (n.charAt(i) > n.charAt(i + 1)) {// if the index of char at i of the String is greater than the next char then rthe method will return false 
-	return false;
-
-	}
-	}
-	return true;
+		if (n.length() == 0) { // if the String is empty then the result is true 
+			return true;
+		}
+		if (n.length() == 1) { // if the String is just one char then the result is true 
+			return true;
+		}
+		for (int i = 0; i < n.length() - 1; i++) {
+			if (n.charAt(i) > n.charAt(i + 1)) {// if the index of char at i of the String is greater than the next char then rthe method will return false 
+				return false;
+			}
+		}
+		return true;
 	}
 }
 
