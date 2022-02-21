2830a6be80ce3d63e3c413b81ba9a7f66a714ff5
Wali Khan <wali.m.khan@emory.edu>
2022-02-21 00:01:59 -0800

    Update Story.java

--- CS170/hw2/Story.java
+++ CS170/hw2/Story.java
@@ -1,8 +1,9 @@
 public class Story{
+	
 	public static void main(String[] args){
 		System.out.println(storyteller("Alissa","Shantanu", "shirts", 56, "mice" , "margarine", "cheeseburger")); // this is the main method which sets up the values which compose the  6 strings and 1 int
-
 	}
+	
 	public static String storyteller(String n1,String n2, String possession, int i, String b, String c,String d){ // this is the story teller method which has 6 strings and 1 int as its parameter
 		String result = "";
 		result += n1 + " was a egocentric princess. ";
@@ -19,8 +20,6 @@ public class Story{
 		" became upset and wanted to eat food, she went to the kitchen in hopes of finding some " + c + " she could not find that so she ended up eating a "
 		 + d + ". After eating food, " + n1 + " decided she had a long day and it was now time to retire to bed.";
 		return result;
-
 	}
-
 
 }
