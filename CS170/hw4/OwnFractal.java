a6b232c2e936fd7b3d1851c93adb06dd988e20b5
Wali Khan <wali.m.khan@emory.edu>
2022-02-20 23:18:49 -0800

    Update OwnFractal.java

--- CS170/hw4/OwnFractal.java
+++ CS170/hw4/OwnFractal.java
@@ -19,28 +19,25 @@ public class OwnFractal{
 	public static void ownFractal(Turtle t, double size, int level){ // this is my own fractal which is supposed to be similar to a tree with random sized branches 
 
 		if (level > 0) { // method keeps calling itself until level reaches 0. 
-
 			double x = Math.random() * 2.5; 
 			t.penup();
 			t.color(250 - (50 * level), 50 * level, 0); // the color is also varied starts at red at the higher levels and goes to green.
 			t.pendown();
-            t.forward(size*x); // brances given a random size. 
-            ownFractal(t, size * 0.8, level-1);
-            t.right(15 * level);
-            t.penup();
+			t.forward(size*x); // brances given a random size. 
+			ownFractal(t, size * 0.8, level-1);
+			t.right(15 * level);
+			t.penup();
 			t.color(250 - (50 * level), 50 * level, 0);
 			t.pendown();
-            ownFractal(t, size * 0.8, level-1); // calls for the branches on the left side. 
-            t.left(30 * level);
-            t.penup();
+			ownFractal(t, size * 0.8, level-1); // calls for the branches on the left side. 
+			t.left(30 * level);
+			t.penup();
 			t.color(250 - (50 * level), 50 * level, 0);
-		 
 			t.pendown();
-            ownFractal(t, size * 0.8, level-1); // calls for the branches on the left side 
-            t.right(15 * level);
-            t.backward(size*x);
+			ownFractal(t, size * 0.8, level-1); // calls for the branches on the left side 
+			t.right(15 * level);
+			t.backward(size*x);
 			t.color(250 - (50 * level), 50 * level, 0);
-
-        }
+		}
 	}
 }
