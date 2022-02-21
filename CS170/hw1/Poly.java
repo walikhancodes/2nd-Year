307b7a4c0b9bb268ea672a5b4d2c1780961da169
Wali Khan <wali.m.khan@emory.edu>
2022-02-21 00:08:11 -0800

    Update Poly.java

--- CS170/hw1/Poly.java
+++ CS170/hw1/Poly.java
@@ -20,16 +20,13 @@ public class Poly{
 		bob.left(90);
 		bob.pendown();
 		polyspiral(bob, 8, 5, 3);
-
-		
-
 	}
 
 	public static void polyspiral(Turtle t, int n, double base, int rounds){ // this is the method for the polyspiral and all it is doing is taking a certain variable distance "base" and multiplying it by a factor of i+1 which makes the base bigger each time it rotates 360.0 / n degrees and the loop is repeated n * rounds amount of times. THis is to ensure the Polyspiral is long and the shape that it should be.
 		for(int i = 0; i < n * rounds; i++){
-		t.forward(base * (i+1));
-		t.right(360/n);
+			t.forward(base * (i+1));
+			t.right(360/n);
 		}
-
 	}
+	
 }
