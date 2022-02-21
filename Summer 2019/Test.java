f60710f65670cda8a5dd03cd69d1f2df566b6638
Wali Khan <wali.m.khan@emory.edu>
2022-02-21 00:22:48 -0800

    Update Test.java

--- Summer 2019/Test.java
+++ Summer 2019/Test.java
@@ -3,7 +3,6 @@ import java.util.Stack;
 public class Test{
 	
 	public static void main(String[] args) {
-
 		Stack<Integer> numPositions = new Stack<Integer>();
 		int j = 4;
 		int n = 4; 
@@ -12,28 +11,27 @@ public class Test{
 		numPositions.push(3);
 		numPositions.push(3);
 		while(j == n){
-        if(numPositions.size() == 1){
-          j = numPositions.pop() + 1;
-          numPositions.push(j);
-          j = 0;
-        } else {
-          j = numPositions.pop() + 1; 
-          i--;
-        }
-        
-        if(numPositions.peek() == n && numPositions.size() == 1){
-            numPositions.pop();
-            i--;
-            j = 0;
-        } else {}
+			if(numPositions.size() == 1){
+				j = numPositions.pop() + 1;
+				numPositions.push(j);
+			  	j = 0;
+			} else {
+			  	j = numPositions.pop() + 1; 
+			  	i--;
+			}
 
-      }
-      for(int k = 0; k < numPositions.size(); k++){
-      	System.out.println(numPositions.get(k));
-      }
-
-      System.out.println(i);
-      System.out.println(j);
-      System.out.println(n);
+			if(numPositions.peek() == n && numPositions.size() == 1){
+			    numPositions.pop();
+			    i--;
+			    j = 0;
+			} else {}
+		}
+	      	for(int k = 0; k < numPositions.size(); k++){
+		      System.out.println(numPositions.get(k));
+	      	}
+	      	System.out.println(i);
+	      	System.out.println(j);
+	      	System.out.println(n);
 	}
+	
 }
